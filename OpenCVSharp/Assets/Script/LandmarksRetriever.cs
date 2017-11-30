555616c808819688bf4593a487836e42989f3a17
Ikiriss <32384371+Ikiriss@users.noreply.github.com>
2017-11-30 10:24:57 +0100

    nothing

--- OpenCVSharp/Assets/Script/LandmarksRetriever.cs
+++ OpenCVSharp/Assets/Script/LandmarksRetriever.cs
@@ -176,6 +176,31 @@ public class LandmarksRetriever : MonoBehaviour {
         lipWidth = Math.Abs((double)landmarks["lipCornerLeftX"] - (double)landmarks["lipCornerRightX"]);
         Debug.Log("lip width :");
         Debug.Log(lipWidth);
+
+
+        try
+        {
+
+            //Pass the filepath and filename to the StreamWriter Constructor
+            StreamWriter sw = new StreamWriter("Assets/Test.txt");
+
+            //Write a line of text
+            sw.WriteLine("Hello World!!");
+
+            //Write a second line of text
+            sw.WriteLine("From the StreamWriter class");
+
+            //Close the file
+            sw.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
+        }
     }
 
     public string PostRequest(NameValueCollection parameters) {
