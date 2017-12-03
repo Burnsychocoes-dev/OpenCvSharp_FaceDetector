5e930e6c638085cb7cbf88168e1c24f43a316226
Ikiriss <32384371+Ikiriss@users.noreply.github.com>
2017-12-03 16:02:32 +0100

    avatar adjustement functions add

--- OpenCVSharp/Assets/Script/Avatar.cs
+++ OpenCVSharp/Assets/Script/Avatar.cs
@@ -2,51 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 using OpenCvSharp;
+using System;
 
 public class Avatar : MonoBehaviour {
 
-    MORPH3D.M3DCharacterManager avatar;
-    List<MORPH3D.FOUNDATIONS.Morph> morphs;
+    private MORPH3D.M3DCharacterManager avatarManager;
+    [SerializeField]
+    private Texture maleBlackHeadSkinTexture;
+    [SerializeField]
+    private Texture maleWhiteHeadSkinTexture;
+    [SerializeField]
+    private Texture maleBlackBodySkinTexture;
+    [SerializeField]
+    private Texture maleWhiteBodySkinTexture;
+    [SerializeField]
+    private Texture femelleBlackHeadSkinTexture;
+    [SerializeField]
+    private Texture femelleWhiteHeadSkinTexture;
+    [SerializeField]
+    private Texture femelleBlackBodySkinTexture;
+    [SerializeField]
+    private Texture femelleWhiteBodySkinTexture;
 
-    enum Taille
+    public enum Taille
     {
         Big,
         Middle,
         little
     }
 
-    struct Eye
+    public enum SkinColor
+    {
+        Black,
+        White
+    }
+
+    public enum Gender
+    {
+        Male,
+        Femelle
+    }
+
+    public struct Eye
     {
         Taille width;
     }
 
-    struct Nose
+    public struct Nose
     {
         Taille width;
         Taille height;
     }
 
-    struct Mouth
+    public struct Mouth
     {
         Taille height;
     }
 
-    struct Hair
+    public struct Hair
     {
+        bool isHairless;
         Taille height;
         Taille length;
     }
 
+    public struct Person
+    {
+        Gender gender;
+        SkinColor skinColor;
+        Color exactSkinColor;
+        Eye eye;
+        Nose nose;
+        Mouth mouth;
+        Hair hair;
+    }
+
+    private Person perso;
+    public Person Perso
+    {
+        get { return perso; }
+    }
+
     void Start()
     {
-        avatar = GetComponent<MORPH3D.M3DCharacterManager>();
-        morphs = avatar.coreMorphs.morphs;
-        //foreach(var morph in morphs)
-        //{
-        //    Debug.Log(morph.name);
-        //}
-        avatar.SetBlendshapeValue("PHMEyesSize", 100);
+        avatarManager = GetComponent<MORPH3D.M3DCharacterManager>();
+        avatarManager.SetBlendshapeValue("PHMEyesSize", 100);
+        ChangeSkinTexture(new Color(0.56f, 0.27f, 0.27f), true, false);
     }
+
+
 
     // Update is called once per frame
     void Update () {
@@ -60,15 +104,106 @@ public class Avatar : MonoBehaviour {
     }
 
 
-    public void ChangeColor(Vec3f color)
+    public void ChangeNose(float noseHeight, float noseWidth, float nostrilThickness)
     {
-        //MORPH3D.FOUNDATIONS.Morph morph = new MORPH3D.FOUNDATIONS.Morph();
-        //System.Predicate<MORPH3D.FOUNDATIONS.Morph> PHMChinWidth = new System.Predicate<MORPH3D.FOUNDATIONS.Morph>();
-        //morphs.Find(PHMChinWidth);
+        // En fonction de noseHeight
+        avatarManager.SetBlendshapeValue("PHMNoseHeight", 100);
+        avatarManager.SetBlendshapeValue("PHMNoseHeight_NEGATIVE_", 100);
+
+        // En fonction de noseWidth
+        avatarManager.SetBlendshapeValue("PHMNoseWidth", 100);
+        avatarManager.SetBlendshapeValue("PHMNoseWidth_NEGATIVE_", 100);
+
+        // En fonction de nostrilThickness
+        avatarManager.SetBlendshapeValue("PHMNostrilWingWidth", 100);
+        avatarManager.SetBlendshapeValue("PHMNostrilWingWidth_NEGATIVE_", 100);
     }
 
-    //private static bool FindMorph(MORPH3D.FOUNDATIONS.Morph obj)
-    //{
-    //    return obj.;
-    //}
+    public void ChangeMouth(float distanceBetweenCenterLipAndButtomLip, float distanceBetweenCenterLipAndTopLip, float distanceBetweenChinAndMouth, float distanceBetweenNoseTipAndMouth, float MouthWidth)
+    {
+        // En fonction de distanceBetweenCenterLipAndButtomLip
+        avatarManager.SetBlendshapeValue("PHMLipLowerSize", 100);
+        avatarManager.SetBlendshapeValue("PHMLipLowerSize_NEGATIVE_", 100);
+
+        // En fonction de distanceBetweenCenterLipAndTopLip
+        avatarManager.SetBlendshapeValue("PHMLipUpperSize", 100);
+        avatarManager.SetBlendshapeValue("PHMLipUpperSize_NEGATIVE_", 100);
+
+        // En fonction de distanceBetweenChinAndMouth and distanceBetweenNoseTipAndMouth
+        avatarManager.SetBlendshapeValue("PHMMouthHeight", 100);
+        avatarManager.SetBlendshapeValue("PHMMouthHeight_NEGATIVE_", 100);
+
+        // En fonction de MouthWidth
+        avatarManager.SetBlendshapeValue("PHMMouthWidth", 100);
+        avatarManager.SetBlendshapeValue("PHMMouthWidth_NEGATIVE_", 100);
+    }
+
+    public void ChangeEyes(float distanceMiddleSourcilCenterEye, float eyeWidth, float distanceBetweenNoseTopAndEyes)
+    {
+        // En fonction de distanceMiddleSourcilCenterEye
+        avatarManager.SetBlendshapeValue("PHMEyesHeight", 100);
+        avatarManager.SetBlendshapeValue("PHMEyesHeight_NEGATIVE_", 100);
+
+        // En fonction de eyeWidth
+        avatarManager.SetBlendshapeValue("PHMEyesSize", 100);
+        avatarManager.SetBlendshapeValue("PHMEyesHeight_NEGATIVE_", 100);
+
+        // En fonction de distanceBetweenNoseTopAndEyes
+        avatarManager.SetBlendshapeValue("PHMEyesWidth", 100);
+        avatarManager.SetBlendshapeValue("PHMEyesWidth_NEGATIVE_", 100);
+    }
+
+    public void ChangeSkinTexture(Color color, bool isMale, bool isWhite)
+    {
+        if(isWhite && isMale)
+        {
+            avatarManager.GetHairMaterial().mainTexture = maleWhiteHeadSkinTexture;
+            avatarManager.GetBodyMaterial().mainTexture = maleWhiteBodySkinTexture;
+        }
+        else if(isWhite && !isMale)
+        {
+            avatarManager.GetHairMaterial().mainTexture = femelleWhiteHeadSkinTexture;
+            avatarManager.GetBodyMaterial().mainTexture = femelleWhiteBodySkinTexture;
+        }
+        else if(!isWhite && isMale)
+        {
+            avatarManager.GetHairMaterial().mainTexture = maleBlackHeadSkinTexture;
+            avatarManager.GetBodyMaterial().mainTexture = maleBlackBodySkinTexture;
+        }
+        else if(!isWhite && !isMale)
+        {
+            avatarManager.GetHairMaterial().mainTexture = femelleBlackHeadSkinTexture;
+            avatarManager.GetBodyMaterial().mainTexture = femelleBlackBodySkinTexture;
+        }
+        avatarManager.GetBodyMaterial().SetColor("_Color", color);
+        avatarManager.GetHairMaterial().SetColor("_Color", color);
+    }
+
+    public void SetHair(bool isHairless)
+    {
+        if(isHairless)
+        {
+            foreach (var hair in avatarManager.GetAllHair())
+            {
+                hair.SetVisibility(false);
+            }
+        }
+    }
+
+    public void SetDressed()
+    {
+        foreach(var dress in avatarManager.GetAllClothing())
+        {
+            dress.SetVisibility(true);
+        }
+    }
+
+    public void SetUndressed()
+    {
+        foreach (var dress in avatarManager.GetAllClothing())
+        {
+            dress.SetVisibility(false);
+        }
+    }
+
 }
