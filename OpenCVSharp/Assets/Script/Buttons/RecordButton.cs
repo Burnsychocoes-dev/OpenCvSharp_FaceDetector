c8fe711dd5ffaad59707a64ac7641bc70c42eeb6
Burnsychocoes <32386624+Burnsychocoes@users.noreply.github.com>
2018-02-09 22:33:10 +0100

    UI v2

---
+++ OpenCVSharp/Assets/Script/Buttons/RecordButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RecordButton : MonoBehaviour {
+    //public string _nextScene = "";
+    public Sprite sprite;
+    protected Sprite spritesave;
+    protected SpriteRenderer spriterender;
+    //protected FadingScene fadingScene;
+    void Start()
+    {
+        //fadingScene = GameObject.Find("menu").GetComponent<FadingScene>();
+        spriterender = GetComponent<SpriteRenderer>();
+        //if (_nextScene.Equals(""))
+        //{
+        //    _nextScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+        //}
+        //Debug.Log("yosh");
+    }
+    protected void OnMouseEnter()
+    {
+
+        if (sprite != null)
+        {
+            spritesave = spriterender.sprite;
+            spriterender.sprite = sprite;
+        }
+        else
+        {
+
+        }
+        //SoundEffectsHelper.Instance.MakeButtonSelectSound();
+    }
+    protected void OnMouseOver()
+    {
+        //rend.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+    }
+    protected void OnMouseExit()
+    {
+        //rend.material.color = Color.white;
+        if (sprite != null)
+        {
+            spriterender.sprite = spritesave;
+        }
+
+    }
+
+
+    protected void OnMouseDown()
+    {
+        //SoundEffectsHelper.Instance.MakeButtonSelectedSound();
+        //float fadeTime = fadingScene.BeginFade(1);
+        //yield return new WaitForSeconds(fadeTime);
+        //SceneManager.LoadScene(_nextScene);
+    }
+}
